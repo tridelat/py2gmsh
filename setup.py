18fb47c8f31499b81007e58a459a9a32539e7a19
Tristan de Lataillade <delataillade.tristan@gmail.com>
2016-12-07 12:07:08 +0000

    [WIP] added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'py2gmsh',
+  packages = ['py2gmsh'],
+  version = '0.1',
+  description = 'Python wrappers to create a gmsh geo file',
+  author = 'Tristan de Lataillade',
+  author_email = 'delataillade.tristan@gmail.com',
+  url = 'https://github.com/tridelat/py2gmsh',
+  download_url = 'https://github.com/tridelat/py2gmsh/tarball/0.0.1',
+  keywords = ['gmsh', 'wrapper'],
+  classifiers = [],
+)
